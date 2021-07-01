05b1f06fd50474f534389cfd0f9a5f3e3045b09b
Gustavo Moraes <gustavomm.contato@gmail.com>
2021-06-30 22:19:24 -0300

    Exercício da loja de games

--- 6. Loja de games(atividade API-30-06)/MinhaLojaDeGames/src/main/java/com/LojaGames/MinhaLojaDeGames/controller/ProdutoController.java
+++ 6. Loja de games(atividade API-30-06)/MinhaLojaDeGames/src/main/java/com/LojaGames/MinhaLojaDeGames/controller/ProdutoController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.LojaGames.MinhaLojaDeGames.model.Produto;
 import com.LojaGames.MinhaLojaDeGames.repository.ProdutoRepository;
 
 @RestController
+@RequestMapping("/produto")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
 	@Autowired
